cf9d2af2d290ace4b9c87f38150b6f348295e2f3
Filip <filip.ahlander@gmail.com>
2021-12-01 07:49:44 +0100

    add solution day 1

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as solutionsDay1 from './solutions/day1.js';
+
+solutionsDay1.solution1A();
+solutionsDay1.solution1B();
---
+++ src/solutions/day1.js
@@ -0,0 +1,21 @@
+import { input } from '../input/day1.js';
+
+const arrayOfNumbers = (input) => input.split('\n').map((x) => Number(x));
+const numbers = arrayOfNumbers(input);
+
+export const solution1A = () => {
+  let increased = 0;
+  for (let i = 1; i<input.length; i++) {
+    if (numbers[i] > numbers[i-1]) increased++;
+  }
+  console.log('solution day 1 a: ', increased);
+}
+
+export const solution1B = () => {
+  let increased = 0;
+  const sum = (n) => numbers[n] + numbers[n-1] + numbers[n-2];
+  for (let i = 3; i<numbers.length; i++) {
+    if (sum(i) > sum(i-1)) increased++;
+  }
+  console.log('solution day 1 b: ', increased);
+}
